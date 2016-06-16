0cfe2156a3374e01b08dea7ee607f6f61cece57a
shsaci <shsaci@yahoo.com.au>
2016-06-16 22:44:47 +1200

    added makeGreen

--- events.js
+++ events.js
@@ -1,28 +1,39 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. This will change the background colour of the first div
   // when you mouse over it.
-  one()
-    
+  one();
+  two();
+
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
 function one () {
   // First, we have to find the element:
-  var one = document.getElementById('one')
+  var one = document.getElementById('one');
 
   // Next, we add an event listener to it:
-  one.addEventListener('mouseenter', makeBlue)
+  one.addEventListener('mouseenter', makeBlue);
 
   // Finally, we add one to make the colour white again
-  one.addEventListener('mouseleave', makeWhite)
+  one.addEventListener('mouseleave', makeWhite);
 }
 
 // CREATE FUNCTION two HERE
+function two () {
+  // Find the element two:
+  var two = document.getElementById('two');
+
+// Add an event listener to it:
+two.addEventListener('mouseenter', makeGreen);
+
+// Add an event listener to make white on mouse leave:
+two.addEventListener('mouseleave', makeWhite);
+}
 
 // CREATE FUNCTION three HERE
 
@@ -30,7 +41,11 @@ function one () {
 
 // Changes the background color of event's target
 function makeBlue (evt) {
-  evt.target.style.backgroundColor = 'blue'
+  evt.target.style.backgroundColor = 'blue';
+}
+
+function makeGreen (evt) {
+  evt.target.style.backgroundColor = 'green';
 }
 
 function makeWhite (evt) {
